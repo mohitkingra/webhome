713ae6a4b3cb58c6117da1eb94890837bc295251
Mohit Kingra <mohitkingra@Mohits-MacBook-Pro.local>
2018-05-16 21:47:03 +0800

    Add Route animation

--- public/js/maps/osmMap.js
+++ public/js/maps/osmMap.js
@@ -1,5 +1,77 @@
 function initOpenMap(){
 
+  //---------------ROUTE ANIMATION---------------//
+
+var locations = [[59.941, 30.327], [55.753, 37.619], [56.837, 60.606], [55.018, 82.957], [52.316, 104.253], [47.983, 106.897], [39.904, 116.386], [22.548, 114.046]];
+
+locations.map(function(l) {
+  return l.reverse();
+});
+
+var route = new ol.geom.LineString(locations)
+    .transform('EPSG:4326', 'EPSG:3857');
+
+var routeCoords = route.getCoordinates();
+var routeLength = routeCoords.length;
+
+var routeFeature = new ol.Feature({
+  geometry: route
+});
+
+var geoMarker = new ol.Feature({
+  type: 'geoMarker',
+  geometry: new ol.geom.Point(routeCoords[0])
+});
+
+var startMarker = new ol.Feature({
+  geometry: new ol.geom.Point(routeCoords[0])
+});
+
+var endMarker = new ol.Feature({
+  geometry: new ol.geom.Point(routeCoords[routeLength - 1])
+});
+
+var styles = {
+  'route': new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      width: 6,
+      color: 'lightgrey'
+    })
+  }),
+  'geoMarker': new ol.style.Style({
+    image: new ol.style.Circle({
+      radius: 7,
+      snapToPixel: false,
+      fill: new ol.style.Fill({
+        color: 'green'
+      }),
+      stroke: new ol.style.Stroke({
+        color: 'white',
+        width: 2
+      })
+    })
+  })
+};
+
+var animating = false;
+var now;
+var startButton = document.getElementById('trans-mongolian');
+
+var vectorLayerN = new ol.layer.Vector({
+  source: new ol.source.Vector({
+    features: [routeFeature, geoMarker, startMarker, endMarker]
+  }),
+  style: function(feature) {
+    // hide geoMarker if animation is active
+    if (animating && feature.get('type') === 'geoMarker') {
+      return null;
+    }
+    return styles[feature.get('type')];
+  }
+});
+
+/////
+
   var aelement = document.getElementById('popbanner');
   var bannerpos0 = ol.proj.fromLonLat([75.2253, 72.00]);
   var bannerpos1 = ol.proj.fromLonLat([35.2253, 66.41]);
@@ -71,7 +143,7 @@ function initOpenMap(){
         layers: [ 
           new ol.layer.Tile({
             source: new ol.source.Stamen({layer: 'watercolor'})
-          }), vectorLayer
+          }), vectorLayer, vectorLayerN
         ],
         target: 'map',
         controls: ol.control.defaults({
@@ -151,5 +223,60 @@ function initOpenMap(){
 
   openMap.on('click', function(evt) {
     displayFeatureInfo(evt.pixel);
-  });
+  }); 
+/*
+openMap.getView().fit(
+    vectorLayerN.getSource().getExtent(), openMap.getSize(),{
+      padding: [30, 5, 5, 5]
+    });
+*/
+var moveFeature = function(event) {
+  var vectorContext = event.vectorContext;
+  var frameState = event.frameState;
+
+  if (animating) {
+    var elapsedTime = frameState.time - now;
+    var index = Math.round(1 * elapsedTime / 1000);
+
+    if (index >= routeLength) {
+      stopAnimation(true);
+      return;
+    }
+
+    var currentPoint = new ol.geom.Point(routeCoords[index]);
+    var feature = new ol.Feature(currentPoint);
+    vectorContext.drawFeature(feature, styles.geoMarker);
+  }
+  // tell OL3 to continue the postcompose animation
+  openMap.render();
+};
+
+function startAnimation() {
+  if (animating) {
+    stopAnimation(false);
+  } else {
+    animating = true;
+    now = new Date().getTime();
+    // hide geoMarker
+    geoMarker.setStyle(null);
+    // just in case you pan somewhere else
+    openMap.on('postcompose', moveFeature);
+    openMap.render();
+  }
+}
+
+function stopAnimation(ended) {
+  animating = false;
+
+  // if animation cancelled set the marker at the beginning
+  var coord = ended ? routeCoords[routeLength - 1] : routeCoords[0];
+  
+  (geoMarker.getGeometry())
+  .setCoordinates(coord);
+  //remove listener
+  openMap.un('postcompose', moveFeature);
+}
+
+startButton.addEventListener('click', startAnimation, false);
+
 }
