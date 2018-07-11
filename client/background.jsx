765f8cf995848352b903a561ee2efeb511f6217f
Mohit Kingra <mohitkingra@Mohits-MacBook-Pro.local>
2018-07-12 01:35:41 +0800

    Add ga id

--- client/background.jsx
+++ client/background.jsx
@@ -3,59 +3,24 @@ import ReactDOM from 'react-dom';
 
 import imgWorldUrl from './../server/public/img/world-whale.jpg';
 
-import gtmParts from 'react-google-tag-manager';
- 
-class GoogleTagManager extends React.Component {
-    componentDidMount() {
-        const dataLayerName = this.props.dataLayerName || 'dataLayer';
-        const scriptId = this.props.scriptId || 'react-google-tag-manager-gtm';
- 
-        if (!window[dataLayerName]) {
-            const gtmScriptNode = document.getElementById(scriptId);
- 
-            eval(gtmScriptNode.textContent);
-        }
-    }
- 
-    render() {
-        const gtm = gtmParts({
-            id: this.props.gtmId,
-            dataLayerName: this.props.dataLayerName || 'dataLayer',
-            additionalEvents: this.props.additionalEvents || {},
-            previewVariables: this.props.previewVariables || false,
-        });
- 
-        return (
-            <div>
-                <div>{gtm.noScriptAsReact()}</div>
-                <div id={this.props.scriptId || 'react-google-tag-manager-gtm'}>
-                    {gtm.scriptAsReact()}
-                </div>
-            </div>
-        );
-    }
-}
- 
-GoogleTagManager.propTypes = {
-    gtmId: React.PropTypes.string.isRequired,
-    dataLayerName: React.PropTypes.string,
-    additionalEvents: React.PropTypes.object,
-    previewVariables: React.PropTypes.string,
-    scriptId: React.PropTypes.string
-};
+import ReactGA from 'react-ga';
+
 
 class Background extends React.Component{
 	constructor(props){
 		super();
 	}
 
+	componentDidMount(){
+		ReactGA.initialize('XXX');
+		ReactGA.pageview(window.location.pathname + window.location.search);
+		console.log(window.location.pathname);
+	}
+
 	render(){
 		return(
-			<div>
-				<GoogleTagManager gtmId='XXX' />
-				<div style={{background: 'url(' + imgWorldUrl + ') no-repeat center', backgroundSize : "100% 100%"}} />
-			</div>
-			);
+			<div style={{background: 'url(' + imgWorldUrl + ') no-repeat center', backgroundSize : "100% 100%"}} />
+		);
 	}
 }
 ReactDOM.render(
